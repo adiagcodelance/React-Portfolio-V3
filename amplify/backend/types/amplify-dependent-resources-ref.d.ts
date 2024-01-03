b111a8312679b41f4205b053fa7c9f2fcb452e07
Aditya Agrawal <43040075+aditya7496@users.noreply.github.com>
2024-01-03 16:29:44 -0400

    updates

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "api21461dcf": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "reactportfoliov3e55aab2b": {
+      "Arn": "string",
+      "CloudWatchEventRule": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
--- src/components/EmailModal.js
+++ src/components/EmailModal.js
@@ -11,7 +11,7 @@ const EmailModal = ({ isOpen, onClose }) => {
     e.preventDefault();
 
     try {
-      const response = await fetch("https://aditya-folio.com/api/send-email", {
+      const response = await fetch("http://aditya-folio.com/api/send-email", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
