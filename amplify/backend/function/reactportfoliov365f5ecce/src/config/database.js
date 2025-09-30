71fba9289907adc6ac34f24c0107b7363fce0ecc
Aditya Agrawal <aditya25agrawal@gmail.com>
2025-09-30 19:36:03 -0300

    fix(lambda): require SSL for RDS Postgres; revert VPC config from CFN to allow code-only deploys via Amplify

--- amplify/backend/function/reactportfoliov365f5ecce/src/config/database.js
+++ amplify/backend/function/reactportfoliov365f5ecce/src/config/database.js
@@ -16,6 +16,12 @@ const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   port: DB_PORT,
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
   logging: false,
 });
 
